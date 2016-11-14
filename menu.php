51fcd6eda8377e28266b9d062350a8582c02c9c5
robinBanquo <caramaschi@hotmail.com>
2016-11-14 12:59:28 +0100

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,32 @@
+
+<div id="menu">
+	<div id="menu_left">
+		<!-- inserer un lien vers l'image de la BDD-->
+		<img id="profile_button" class="big_menu_button" src="views/images/spartacus.jpg" />
+	
+		<span id="my_projects_button" class="big_menu_button">Projets</span> 
+		<span id="agenda_button" class="big_menu_button">Agenda</span>
+		<span id="resources_button" class="big_menu_button">Ressources</span>
+		<span id="moderation_button" class="big_menu_button">Modération</span>
+	</div>
+	<div id="menu_right">
+		<div id="menu_top">
+			<form method="post" action="">
+				<!-- à voir -->
+				<input type="text" name="search" placeholder="Recherche" />
+			</form>
+			<span id=profile_button> <img src="views/images/logo_hub.gif" />
+			</span>
+			<!-- image à retravailler car pas jolie -->
+		</div>
+		<div id="menu_expanded">
+			<div>
+					<!-- il faudra une boucle PHP pour lister les projets de l'utilisateur, ainsi que -->
+					<span class=sub_menu_project><a href= "lien vers la page du projet">projet 1</a></span>
+					<span class=sub_menu_project><a href= "lien vers la page du projet">projet 1</a></span>
+					<span class=sub_menu_project><a href= "lien vers la page du projet">projet 1</a></span>
+					<span class=sub_menu_project><a href= "lien vers la page du projet">projet 1</a></span>
+			</div>
+		</div>
+	</div>
+</div>
