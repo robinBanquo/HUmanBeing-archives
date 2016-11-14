d3dd37f766cde3303ddbde3f3d59fe4298a9f212
robinBanquo <caramaschi@hotmail.com>
2016-11-14 12:57:58 +0100

    Add files via upload

---
+++ head.php
@@ -0,0 +1,6 @@
+
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="content-type">
+<title>HUmanBeing</title>
+<?php include "views/css/css_sheets_list.php";?> <!-- on ajoute toutes les feuilles de styles créées -->
+</head>
