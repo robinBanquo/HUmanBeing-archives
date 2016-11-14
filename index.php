5ba45189e72f136dffd573194454bbe74d364817
robinBanquo <caramaschi@hotmail.com>
2016-11-14 12:40:38 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+	<?php include("views/head.php") ; ?> <!-- chargement de tout le head de la page -->
+	<body>
+		<?php include("views/menu.php") ; ?> <!-- chargement de la page -->
+	</body>
+</html>
