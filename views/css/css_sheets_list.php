2952a031c97637778758703568fc9ae49ff0a284
robinBanquo <caramaschi@hotmail.com>
2016-11-15 13:47:55 +0100

    envoi du début du travail
    mise a jour initiale

---
+++ views/css/css_sheets_list.php
@@ -0,0 +1,5 @@
+<!-- on liste ici toutes les feuilles de styles de chaques element contenus dans la page, 
+peut etre sera t'il nécessaire de créer du code pour charger uniquement celles qui sont nécessaires -->
+
+<link rel="stylesheet" type="text/css" href="views/css/general.css">
+<link rel="stylesheet" type="text/css" href="views/css/menu.css">
--- views/menu.php
+++ views/menu.php
@@ -2,18 +2,24 @@
 <div id="menu">
 	<div id="menu_left">
 		<!-- inserer un lien vers l'image de la BDD-->
-		<img id="profile_button" class="big_menu_button" src="views/images/spartacus.jpg" />
-	
+		<img id="profile_button" class="big_menu_button" alt="avatar" src="views/images/spartacus.jpg" />
+        <span id="mail_info" class="big_menu_button">
+        <img alt="messagerie" src="views/images/message_menu.png"/>
+        </span>       
 		<span id="my_projects_button" class="big_menu_button">Projets</span> 
 		<span id="agenda_button" class="big_menu_button">Agenda</span>
 		<span id="resources_button" class="big_menu_button">Ressources</span>
+        <span id="ressources_info" class= "big_menu_button">3</span> <!-- petit indicateur lié au ressources pour visualiser si ya du nouveau -->
 		<span id="moderation_button" class="big_menu_button">Modération</span>
+        <span id="moderation_info_1" class= "big_menu_button">1</span> <!-- petit indicateur lié a la modération pour visualiser si ya du nouveau -->
+        <span id="moderation_info_2" class= "big_menu_button">6</span> <!-- autre petit indicateur lié à la modération pour visualiser si ya du nouveau -->
 	</div>
 	<div id="menu_right">
 		<div id="menu_top">
 			<form method="post" action="">
 				<!-- à voir -->
-				<input type="text" name="search" placeholder="Recherche" />
+				<input id="menu_search_tool" type="text" name="search" placeholder="Recherche" />
+                <input id="submit_search" type="submit"/> 
 			</form>
 			<span id=profile_button> <img src="views/images/logo_hub.gif" />
 			</span>
