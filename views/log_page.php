89b8b43e1d18449828fc8c37b316fab1c5e77454
robinBanquo <caramaschi@hotmail.com>
2016-11-16 23:54:17 +0100

    avancées du jour
    toujours sur la page d'acceuil et sur le formulaire login

--- views/log_page.php
+++ views/log_page.php
@@ -11,14 +11,22 @@
 		<?php include("views/base_page.php"); ?> 
 
 		<div id="info_search">
+
 			vous pouvez effectuer des recherches parmis les profils utilisateurs et les projets qui ont fait le choix d'etre visibles aux utilisateurs non-inscrits
 		</div>
 
-
-		<div id="connexion">
-				<div id="bienvenue_text">
-					texte de présentation de human being
-				</div>
+		<div id="bienvenue_text">
+			<h1>Bienvenue sur HUB-HUmanBeing</h1>
+			<ul>
+				<li>Une plateforme de partage, d’entraide et de collaboration.</li>
+				<li> Une mise en lien des personnes au travers de projets contribuant au bien-être d’une communauté humaine, et non une mise en lien des utilisateurs sur des bases de communautés idéologiques.</li>
+				<li>Un logiciel pour tous : gratuité totale, ergonomie très accessible…</li>
+				<li>De l’open source pour que le logiciel puisse évoluer au gré des besoins de tous.</li>
+				<li>Un système permettant à tous les utilisateurs d’être acteurs de la modération.</li>
+			</ul>
+		</div>
+			<div id="connexion">
+				
 			<div id="log_in">
 				<input type="button" name="connect">
 			</div>
