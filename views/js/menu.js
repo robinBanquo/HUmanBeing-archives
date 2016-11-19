6d9cd0d8ab3cb92715102b9a86623425830361aa
robinBanquo <caramaschi@hotmail.com>
2016-11-19 19:46:25 +0100

    petits ajouts

--- views/js/menu.js
+++ views/js/menu.js
@@ -3,25 +3,32 @@ $(document).ready(function(){
 
 	$(".menu_expanded").hide();//on cache initialement tout les sous menus.
 
-//Affichage des sous-menus en cliquant sur les boutons correspondants
+//#########Affichage des sous-menus en cliquant sur les boutons correspondants et animations associées##########
+
+	var $initial_text_shadow = $(".big_menu_button").css("text-shadow"); //on met dans une variable la couleur initiale de l'ombre des boutons
+
 	$("#profile_button").click(function(){
 		$(".menu_expanded").hide();//on commence par fermer ceux qui seraient éventuelleme,t déja ouverts
 		$("#profile_menu").show();
+		$(".big_menu_button").css("text-shadow", $initial_text_shadow);//on réinitialise la couleur de tout les bouttons
 	});
 	$("#my_projects_button").click(function(){
 		$(".menu_expanded").hide();
 		$("#my_projects_menu").show();
+		$(".big_menu_button").css("text-shadow", $initial_text_shadow);//on réinitialise la couleur de tout les bouttons
+		$( this ).css("text-shadow", "0 0 30px rgba(0,0,200,1)");
 	});
 	$("#ressources_button").click(function(){
 		$(".menu_expanded").hide();
 		$("#ressources_menu").show();
+		$(".big_menu_button").css("text-shadow", $initial_text_shadow);//on réinitialise la couleur de tout les bouttons
+		$( this ).css("text-shadow", "0 0 30px rgba(200,0,0,0.7)");
 	});
 	$("#moderation_button").click(function(){
 		$(".menu_expanded").hide();
 		$("#moderation_menu").show();
+		$(".big_menu_button").css("text-shadow", $initial_text_shadow);//on réinitialise la couleur de tout les bouttons
+		$( this ).css("text-shadow", "0 0 30px rgba(0,200,0,0.7)");
 	});
-
-
-
 });
 
