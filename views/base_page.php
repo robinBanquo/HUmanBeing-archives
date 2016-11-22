30b5ac2db9a5a3bb9fb021d48595a1acf08cb208
robinBanquo <caramaschi@hotmail.com>
2016-11-22 15:19:06 +0100

    responsive design

--- views/base_page.php
+++ views/base_page.php
@@ -1,14 +1,12 @@
 <!--page regroupant tout les élement invariants du site -->
-<div id="search_menu">
 	<div id="general_header">
 		<form method="post" action="">
 			<!-- à voir -->
 			<input id="menu_search_tool" type="text" name="search" placeholder="Recherche" />
-            <input id="search_button" type="image" src="views/images/loupe.png" /> 
+            <input id="search_button" type="image" src="views/images/loupe.png" />
 		</form>
-		<span id=homepage> 
+		<span id=homepage>
 			<input type='image' src="views/images/logo_hub.gif" /><!-- image à retravailler car pas jolie -->
 		</span>
-		
+
 	</div>
-</div>
--- views/navigation.php
+++ views/navigation.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html>
+
 	<?php include("views/head.php") ; ?> <!-- chargement de tout le head de la page -->
 	<body>
-			<?php
-		include("views/js/scripts_list.php");//les scripts JS de la page
-		?>
-		<?php 
-		include("views/base_page.php");//l'outil recherche et le logo
-		include("views/menu.php") ; //le menu de gauche
-		?> 
+
+			<div id="global_container">
+				<?php
+					include("views/js/scripts_list.php");//les scripts JS de la page
+				?>
+				<?php
+					include("views/base_page.php");//l'outil recherche et le logo
+					include("views/menu.php") ; //le menu de gauche
+				?>
+			</div>
 
 	</body>
 
